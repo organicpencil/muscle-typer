328c847d4bd57bd9d55c6012d4343a7ea1eac213
organicpencil <4343398+organicpencil@users.noreply.github.com>
2019-10-06 15:43:15 -0500

    Export fixes

--- project/global.gd
+++ project/global.gd
@@ -18,16 +18,28 @@ func load_grunts():
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
 		while file_name != "":
+			print(file_name)
 			if file_name.ends_with(".wav"):
 				filepaths.append("res://grunts/" + file_name)
 				
 			file_name = dir.get_next()
+			
+		if filepaths.size() == 0:
+			# Export workaround
+			dir.list_dir_begin()
+			file_name = dir.get_next()
+			while file_name != "":
+				if file_name.ends_with(".import"):
+					file_name = file_name.replace(".import", "")
+					filepaths.append("res://grunts/" + file_name)
+					
+				file_name = dir.get_next()
 			
 		filepaths.sort()
 		for f in filepaths:
 			grunts.append(load(f))
 	else:
 		print("Error accessing grunts folder")
-	
+		
 func _on_crack():
 	cracked = true
