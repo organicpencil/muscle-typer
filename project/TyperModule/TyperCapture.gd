c585d18e3a4d2f25a2f26f645adba3f8870319ca
organicpencil <4343398+organicpencil@users.noreply.github.com>
2019-10-05 19:01:29 -0500

    Progress lifting and size as you type

--- project/TyperModule/TyperCapture.gd
+++ project/TyperModule/TyperCapture.gd
@@ -5,8 +5,13 @@ var levelPhrases
 var currentPhrase
 var storedPhrase
 var level = 1
+var total_phrases = 30
+
+export(NodePath) var player
 
 func _ready():
+	assert(player != null)
+	player = get_node(player)
 	_retrieve_json()
 
 ## retrieves json
@@ -33,6 +38,7 @@ func _get_level_phrases(newDict):
 				levelPhrases = json[i]
 	# picks a random phrase from an array
 	currentPhrase = levelPhrases[(randi() % levelPhrases.size())]
+	$CurrentPhrase.text = currentPhrase
 	print(currentPhrase)
 
 ##
@@ -71,6 +77,14 @@ func _check_complete(checks):
 			if order == false:
 				return false
 		return true
+		
+func _check_accuracy(phrase : String):
+	var failures = 0
+	for i in range(0, phrase.length()):
+		if i >= currentPhrase.length() or phrase[i] != currentPhrase[i]:
+			failures += 1
+			
+	return int(max(failures, 0))
 
 ## text is stored and changed
 func _on_TextEdit_text_changed():
@@ -80,14 +94,20 @@ func _on_TextEdit_text_changed():
 	else:
 		storedPhrase = phrase
 	_parse_text(phrase);
-	pass
+	
+	var failures = _check_accuracy(phrase)
+	player.lift_percent = float(phrase.length() - failures) / float(currentPhrase.length())
 
 ## On click of enter it checks if its success or failure
 func _on_TextEdit__on_submit():
 	if _check_complete(_parse_text(storedPhrase)):
 		print("Success!")
+		player.lift_success()
 	else:
 		print("Failure")
+		player.lift_failure()
+	player.arm_size += 1.0 / total_phrases
 	currentPhrase = levelPhrases[(randi() % levelPhrases.size())]
+	$CurrentPhrase.text = currentPhrase
 	print(currentPhrase)
 	pass
--- project/player/player.gd
+++ project/player/player.gd
@@ -6,15 +6,18 @@ var _anim_grow
 var _lifting = false
 var _delayed_signal = false
 var _weights = 0
+var _lift_percent_lerp = 0.0
+var _arm_size_lerp = 0.0
 
 ## Important vars
-var lift_speed = 0.0 # Set me to determine lifting speed. Normal = 1.0
-var arm_size = 0.0 # Ranges from 0.0 to 4.0
+#var lift_speed = 0.0 # Set me to determine lifting speed. Normal = 1.0
+var arm_size = 0.0 # Ranges from 0.0 to 1.0
+var lift_percent = 0.0 # Ranges from 0.0 to 1.0, should match percent of letters typed
 
 ### Signals that might be important for sound and scoring
 signal lift_start
 signal lift_end
-signal lift_start_delayed
+signal delayed_grunt
 
 const WEIGHT_SCENE = preload("weight.dae")
 
@@ -40,23 +43,30 @@ func add_weight():
 	
 	_weights += 1
 	
+func lift_success():
+	_lifting = false
+	_anim_lift.play("lift_down", 0.2)
+	lift_percent = 0.0
+	
+func lift_failure():
+	_anim_lift.play("lift_down", 0.2)
+	lift_percent = 0.0
+	
 func _process(delta):
+	_lift_percent_lerp = lerp(_lift_percent_lerp, lift_percent, 0.1)
+	_arm_size_lerp = lerp(_arm_size_lerp, arm_size, 0.01)
+	
 	if !_anim_lift.is_playing():
-		if _lifting:
-			_lifting = false
-			_anim_lift.play("lift_down")
-			emit_signal("lift_end")
-		else:
-			_lifting = true
-			_delayed_signal = false
-			_anim_lift.play("lift_up", -1, lift_speed)
-			emit_signal("lift_start")
-			
-	elif _anim_lift.current_animation == "lift_up":
-		_anim_lift.playback_speed = lift_speed
+		_lifting = true
+		_delayed_signal = false
+		_anim_lift.play("lift_up", -1, 0.0)
+		emit_signal("lift_start")
 		
-		if !_delayed_signal and _anim_lift.current_animation_position > 0.15:
+	elif _anim_lift.current_animation == "lift_up":
+		_anim_lift.seek(_lift_percent_lerp * 0.7)
+		
+		if !_delayed_signal and _anim_lift.current_animation_position > 0.5:
 			_delayed_signal = true
-			emit_signal("lift_start_delayed")
+			emit_signal("delayed_grunt")
 			
-	_anim_grow.seek(arm_size)
+	_anim_grow.seek(_arm_size_lerp * 4.0) # The animation is 4 seconds long
